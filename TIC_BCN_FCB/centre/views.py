e922d1effa573c2c11a6572187ca54436faa308f
Facundo Barrios <cf22facundo.barrios@iesjoandaustria.org>
2024-03-08 19:32:24 +0100

    me olvide el commit de hoy a las 17.30

--- TIC_BCN_FCB/centre/views.py
+++ TIC_BCN_FCB/centre/views.py
@@ -9,10 +9,27 @@ def index(request):
     return HttpResponse(template.render())
 
 def students(request):
-    return HttpResponse("Desde Alumnos")
+    alumnos = [
+        {"name":"Kevin","surname":"Arata","email":"kevin@itic.com"},
+        {"name":"Facundo","surname":"Barrios","email":"facundo@itic.com"},
+        {"name":"Angelo","surname":"Montenegro","email":"angelo@itic.com"},
+        {"name":"Neus","surname":"Bravo","email":"neus@itic.com"},
+        {"name":"Adrià","surname":"Garcçia","email":"adria@itic.com"},
+        {"name":"Gemma","surname":"Garrigosa","email":"gemma@itic.com"},
+        {"name":"Oscar","surname":"Perez","email":"oscar@itic.com"},
+        {"name":"Larry","surname":"Medino","email":"larry@itic.com"},
+        {"name":"Kevin again","surname":"Arata","email":"kevin2@itic.com"},
+    ]
+    context = {'alumnos': alumnos}
+    return render(request, 's_index.html', context )
 
 def teachers(request):
-    profe = {"name":"roger","surname":"sobrino","age":"17"}
-    # template = loader.get_template('index.html')
-    # dades = template.render()
-    return render(request, 'p_index.html', {'nombre':profe['name'],'apellido':profe['surname'],'edad':profe['age']})
+    profesores = [
+        {"name":"Juan Manuel","surname":"Sanchez","module":"M06"},
+        {"name":"Roger","surname":"Sobrino","module":"M07"},
+        {"name":"Xavi","surname":"Quesada","module":"M08"},
+        {"name":"Oriol","surname":"Roca","module":"M09"},
+    ]
+    context = {'profes': profesores}
+    #objeto context
+    return render(request, 't_index.html', context )
