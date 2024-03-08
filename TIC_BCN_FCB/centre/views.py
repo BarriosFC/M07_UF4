27f650065c877d7008fbb239c033a36485bfa2d0
Facundo Barrios <cf22facundo.barrios@iesjoandaustria.org>
2024-03-08 16:27:08 +0100

    marzo 8, primera hora

--- TIC_BCN_FCB/centre/views.py
+++ TIC_BCN_FCB/centre/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def students(request):
+    return HttpResponse("Desde Alumnos")
+
+def teachers(request):
+    return HttpResponse("Desde Profes")
