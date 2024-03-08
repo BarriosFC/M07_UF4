e1b7d407634ddaeabf360ebc941bafec909c0127
Facundo Barrios <cf22facundo.barrios@iesjoandaustria.org>
2024-03-08 17:02:40 +0100

    marzo 8, 17hs

--- TIC_BCN_FCB/centre/views.py
+++ TIC_BCN_FCB/centre/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import Context, loader
 
 # Create your views here.
 
@@ -12,4 +12,7 @@ def students(request):
     return HttpResponse("Desde Alumnos")
 
 def teachers(request):
-    return HttpResponse("Desde Profes")
+    profe = {"name":"roger","surname":"sobrino","age":"17"}
+    # template = loader.get_template('index.html')
+    # dades = template.render()
+    return render(request, 'p_index.html', {'nombre':profe['name'],'apellido':profe['surname'],'edad':profe['age']})
